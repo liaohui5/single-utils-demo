69866f86124d542653bd3546f2d48866bb70fdf4
secret <secretx500@outlook.com>
2025-01-13 00:33:15 +0800

    docs(release): 更新 release 配置并添加 README
    - 更新 release.config.js，增加插件配置以生成 CHANGELOG.md 并发布到 npm 仓库
    - 添加 README.md 文件，简要介绍项目自动化发布版本的功能
    - 修改 GitHub Actions 工作流，移除 NPM_TOKEN 注释

---
+++ README.md
@@ -0,0 +1,3 @@
+## 介绍
+
+使用 GitHub Actions 和 semantic-release 自动化发布版本测试代码
--- release.config.js
+++ release.config.js
@@ -4,23 +4,40 @@
 export default {
 	branches: ["main", "next"],
 	plugins: [
-		// 安装(pnpm i -D semantic-release) 后, 默认会有这4个插件
+		// 1.安装(pnpm i -D semantic-release) 后, 默认会有这4个插件
 		// 分析提交记录的 conventional-changelog, 用于生成变更日志
 		// https://github.com/semantic-release/commit-analyzer
 		"@semantic-release/commit-analyzer",
 
-		// 使用 conventional-changelog 生成变更日志内容
+		// 2.使用 conventional-changelog 生成变更日志内容
 		// https://github.com/semantic-release/release-notes-generator
 		"@semantic-release/release-notes-generator",
 
-		// 如果需要给插件传入参数, 可以使用数组形式,
+		// 3.每个发布步骤都是通过可配置的插件实现的, 这允许支持不同的提交消息格式、
+		// 发布说明生成器和发布平台, 甚至你可以用它来发布 cargo/pip/composer 的包
+		// 为了实现更方便的功能, 可以去官网找更多需要的插件:
+		// https://semantic-release.gitbook.io/semantic-release/extending/plugins-list
+		[
+			// 将 Github Actions 生成的提交记录
+			// 写入到 CHANGELOG.md 文件中
+			"@semantic-release/changelog",
+			{
+				changelogFile: "CHANGELOG.md",
+			},
+		],
+
+		// 3.如果需要给插件传入参数, 可以使用数组形式,
 		// 数组第一个元素是插件名称, 后面是插件参数
 		// 默认的参数请查看对应插件仓库的 README.md
 		// https://github.com/semantic-release/npm
 		[
 			"@semantic-release/npm",
 			{
-				npmPublish: false,
+				// npmPublish: 是否发布到 npm 仓库,
+				// 如果设置为 true:
+				// 1.注意需要配置 github secrets
+				// 2.会自动修改 package.json 的 version 字段
+				npmPublish: true,
 				tarballDir: "dist",
 			},
 		],
@@ -32,10 +49,5 @@ export default {
 				assets: ["dist", "CHANGELOG.md"],
 			},
 		],
-
-		// 每个发布步骤都是通过可配置的插件实现的, 这允许支持不同的提交消息格式、
-		// 发布说明生成器和发布平台, 甚至你可以用它来发布 cargo/pip/composer 的包
-		// 为了实现更方便的功能, 可以去官网找更多需要的插件:
-		// https://semantic-release.gitbook.io/semantic-release/extending/plugins-list
 	],
 };
